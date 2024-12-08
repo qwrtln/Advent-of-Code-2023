ad8a8d4fb4c9fd473d96eca35b8b1f1f1126f5d9
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2024-12-08 16:57:13 +0100

    Simplify day 8

--- 2024/08.py
+++ 2024/08.py
@@ -8,13 +8,11 @@ WIDTH = len(puzzle[0])
 
 
 ANTENNAS_MAPPING = collections.defaultdict(list)
-LOCATION_MAPPING = {}
 
 for y in range(len(puzzle)):
     for x in range(len(puzzle[y])):
         if puzzle[y][x] != ".":
             ANTENNAS_MAPPING[puzzle[y][x]].append((y, x))
-            LOCATION_MAPPING[(y, x)] = puzzle[y][x]
 
 
 def get_neighbouring_antinodes(p1, p2):
@@ -41,37 +39,32 @@ def get_all_antinode_positions(p1, p2):
     a_y_1, a_y_2, a_x_1, a_x_2 = [], [], [], []
     if p1[0] < p2[0]:
         dy = p2[0] - p1[0]
-        a_y_1 = list(range(p1[0], -1, -dy))
-        a_y_2 = list(range(p2[0], HEIGHT, dy))
+        a_y_1 = range(p1[0], -1, -dy)
+        a_y_2 = range(p2[0], HEIGHT, dy)
     else:
         dy = p1[0] - p2[0]
-        a_y_1 = list(range(p1[0], HEIGHT, dy))
-        a_y_2 = list(range(p2[0], -1, -dy))
+        a_y_1 = range(p1[0], HEIGHT, dy)
+        a_y_2 = range(p2[0], -1, -dy)
     if p1[1] < p2[1]:
         dx = p2[1] - p1[1]
-        a_x_1 = list(range(p1[1], -1, -dx))
-        a_x_2 = list(range(p2[1], WIDTH, dx))
+        a_x_1 = range(p1[1], -1, -dx)
+        a_x_2 = range(p2[1], WIDTH, dx)
     else:
         dx = p1[1] - p2[1]
-        a_x_1 = list(range(p1[1], WIDTH, dx))
-        a_x_2 = list(range(p2[1], -1, -dx))
-    for y, x in zip(a_y_1, a_x_1):
-        yield (y, x)
-    for y, x in zip(a_y_2, a_x_2):
-        yield (y, x)
-    yield p1
-    return p2
+        a_x_1 = range(p1[1], WIDTH, dx)
+        a_x_2 = range(p2[1], -1, -dx)
+    yield from zip(a_y_1, a_x_1)
+    yield from zip(a_y_2, a_x_2)
+    yield from (p1, p2)
 
 
 def point_in_map(p):
-    if p[0] in range(HEIGHT) and p[1] in range(WIDTH):
-        return True
-    return False
+    return p[0] in range(HEIGHT) and p[1] in range(WIDTH)
 
 
 def count_antinodes(mapping, neighbouring_func):
     anti_positions = set()
-    for node, positions in mapping.items():
+    for positions in mapping.values():
         for p1, p2 in itertools.combinations(positions, 2):
             for node in neighbouring_func(p1, p2):
                 if point_in_map(node):
